346ef7eca2c9a9ad661da076edbe9cbc878fa517
smadhumithaa <112147052+smadhumithaa@users.noreply.github.com>
2022-09-14 14:17:18 +0530

    Create integerInnum.java

---
+++ Sorting/beginnersAssignment/integerInnum.java
@@ -0,0 +1,10 @@
+class Main {
+public static void main(String args[]){
+  Scanner sc=new Scanner(System.in);
+  int n=10315;
+  while(n!=0){
+      System.out.println(n%10);
+      n=n/10;
+  }
+	}
+}
