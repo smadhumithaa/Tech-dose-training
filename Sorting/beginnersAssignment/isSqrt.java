89c70adf8f5e64a0a2fa0a35b637ce6768ceaffe
smadhumithaa <112147052+smadhumithaa@users.noreply.github.com>
2022-09-14 14:25:17 +0530

    Create isSqrt.java

---
+++ Sorting/beginnersAssignment/isSqrt.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+class Main {
+public static void main(String args[]){
+  Scanner sc=new Scanner(System.in);
+  int n=100;
+  double s=Math.sqrt(n);
+  if(s-Math.floor(s)) == 0)
+    System.out.print("Yes");
+  else
+    System.out.print("No);
+	}
+}
