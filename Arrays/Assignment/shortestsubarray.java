352f27f9de5f870e15e3567ee0443a81a224548e
smadhumithaa <112147052+smadhumithaa@users.noreply.github.com>
2022-10-02 21:40:55 +0530

    Create shortestsubarray.java

---
+++ Arrays/Assignment/shortestsubarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int shortestSubarray(int[] A, int K) {
+        int N = A.length;
+        long[] P = new long[N+1];
+        for (int i = 0; i < N; ++i)
+            P[i+1] = P[i] + (long) A[i];
+        int ans = N+1;
+        Deque<Integer> monoq = new LinkedList(); 
+        for (int y = 0; y < P.length; ++y) {
+            while (!monoq.isEmpty() && P[y] <= P[monoq.getLast()])
+                monoq.removeLast();
+            while (!monoq.isEmpty() && P[y] >= P[monoq.getFirst()] + K)
+                ans = Math.min(ans, y - monoq.removeFirst());
+            monoq.addLast(y);
+        }
+        return ans < N+1 ? ans : -1;
+    }
+}
