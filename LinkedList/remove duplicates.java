49c068e827953ab8c1aa15eb1dea4cba6cfc14af
smadhumithaa <112147052+smadhumithaa@users.noreply.github.com>
2022-10-27 10:39:43 +0530

    Create remove duplicates.java

---
+++ LinkedList/remove duplicates.java
@@ -0,0 +1,13 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp=head;
+        while(temp!=null){
+            if(temp.next!=null && temp.val==temp.next.val){
+                temp.next=temp.next.next;
+            }
+            else
+                temp=temp.next;
+        }
+        return head;
+    }
+}
