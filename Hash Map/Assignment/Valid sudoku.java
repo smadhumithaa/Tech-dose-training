337cb9e2d8c09bf4833ee314b8a9c4db67fa4b7f
smadhumithaa <112147052+smadhumithaa@users.noreply.github.com>
2022-10-31 09:19:38 +0530

    Create Valid sudoku.java

---
+++ Hash Map/Assignment/Valid sudoku.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char n = board[i][j];
+
+                if (n != '.') {
+                    if (set.contains(n + "r" + i) || set.contains(n + "c" + j)
+                        || set.contains(n + "b" + i / 3 + j / 3)) {
+                        System.out.println(n + "r" + i);
+                        System.out.println(n + "c" + j);
+                        System.out.println(n + "b" + i / 3 + j / 3);
+                        return false;
+                    }
+
+                    set.add(n + "r" + i);
+                    set.add(n + "c" + j);
+                    set.add(n + "b" + i / 3 + j / 3);
+                }
+            }
+        }
+
+        return true;
+    }
+}
