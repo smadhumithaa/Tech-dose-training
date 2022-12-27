21d7205287d65c5629d03439d4b565f2a2ec14b2
smadhumithaa <112147052+smadhumithaa@users.noreply.github.com>
2022-12-27 12:28:35 +0530

    Create Sort character according to the frequency.java

---
+++ HEAP/Assignments/Sort character according to the frequency.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String frequencySort(String s) {
+       /* PriorityQueue<Map.Entry<Character,Integer>>q1=new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
+        Map<Character,Integer> mp=new HashMap();
+        for(char c:s.toCharArray())
+            mp.put(c,mp.getOrDefault(c,0)+1);
+        for(Map.Entry m:mp.entrySet())
+             q1.add(m);
+    }
+    StringBuilder sb=new StringBuilder();
+    while(q1.size()>0)
+        {
+            Map.Entry element=q1.remove();
+            for(int i=0;i<(int)element.getValue();i++)
+            {
+                sb.append(element.getKey());
+            }
+        }
+        return sb.toString();*/
+    PriorityQueue<Map.Entry<Character,Integer>>q1=new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
+        Map<Character,Integer> t1=new HashMap();
+        for(char c:s.toCharArray())
+            t1.put(c,t1.getOrDefault(c,0)+1);
+        for(Map.Entry m:t1.entrySet())
+             q1.add(m);
+        StringBuilder b1=new StringBuilder();
+        while(q1.size()>0)
+        {
+            Map.Entry element=q1.remove();
+            for(int i=0;i<(int)element.getValue();i++)
+            {
+                b1.append(element.getKey());
+            }
+        }
+        return b1.toString();
+        
+}
+}
